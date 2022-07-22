42583a579032b115ffec9f7cb044d7ec6e8a95ec
Rafael Garcia <rafael_ggarcia@outlook.com>
2022-07-21 22:47:19 -0300

    daando inicio a POO

---
+++ Python-Lessons/poo.py
@@ -0,0 +1,30 @@
+"""
+- Poo é um paradigma de programação que utiliza o mapeamento de objetos do mundo real para modelos computacionais.
+
+- Paradigma de programação é a forma/metodologia utilizada para pensar/desenvolver sistemas.
+
+Principais elementos de POO:
+- Classe -> Modelo do objeto do mundo rea representado computacionalmente
+- Atributo -> Características do objeto.
+- Método -> Comportamento do objeto. (função inclusa dentro da classe).
+- Construtor -> Método especial utilizado para criar os objetos.
+- Objeto -> Instância da classe. 
+
+"""
+
+numero = 10
+
+print(numero)
+print(type(numero))
+
+nome = 'Geek'
+print(nome)
+print(type(nome))
+
+
+class Produto:
+    pass
+
+ps4 = Produto() 
+print(ps4)
+print(type(ps4))
